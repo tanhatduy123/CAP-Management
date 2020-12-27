97a6d294d40b549cb4c7dc451abc66e9adcd14c6
tanhatduy123 <47852094+tanhatduy123@users.noreply.github.com>
2020-12-27 15:23:04 +0700

    Tao project

---
+++ CAP-Management/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace CAP_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/Home/Index" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        /// <summary>
+        /// Send an OpenID Connect sign-out request.
+        /// </summary>
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    CookieAuthenticationDefaults.AuthenticationType);
+        }
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
